2fda5bc2db70cd34436ec9fd9a90243c8f84a581
Erick Díaz <82739201+erickdc7@users.noreply.github.com>
2023-10-28 16:41:49 -0500

    gsap animation

--- assets/js/main.js
+++ assets/js/main.js
@@ -40,3 +40,12 @@ const bgHeader = () => {
 window.addEventListener('scroll', bgHeader)
 
 /*=============== GSAP ANIMATION ===============*/
+gsap.from('.home__shadow', 1.5, { opacity: 0, y: -300, delay: .2 })
+gsap.from('.home__points', 1.5, { opacity: 0, y: -300, delay: .4 })
+gsap.from('.home__grass', 1.5, { opacity: 0, y: 300, delay: .2, ease: 'elastic.out(1, .5)' })
+gsap.from('.home__pumpkin', 1.5, { opacity: 0, y: -300, delay: .6, ease: 'bounce.out' })
+gsap.from('.home__stone', 1.5, { opacity: 0, y: 300, delay: .8, ease: 'elastic.out(1, .5)' })
+gsap.from('.home__moon', 1.5, { opacity: 0, y: 300, delay: 1.5, ease: 'elastic.out(1, .5)' })
+gsap.from('.home__titles', 1.5, { opacity: 0, y: -300, delay: 1.5, ease: 'elastic.out(1, .5)' })
+gsap.from('.home__tree', 1.5, { opacity: 0, y: 300, delay: 2, ease: 'elastic.out(1, .5)' })
+gsap.from('.home__data', 1.5, { opacity: 0, y: 200, delay: 2, ease: 'elastic.out(1, .5)' })
